941d969881d4e8af1e368b59f89efefe6108def7
Yamato <wonka.bm@gmail.com>
2017-03-05 14:55:19 +0900

    change again application.rb to omit unneeded test files when making controllers

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,9 @@ Bundler.require(*Rails.groups)
 module Chatspace
   class Application < Rails::Application
     config.generators do |g|
-      g.helper false
-      g.assets false
+      g.test_framework nil
+      g.helper         false
+      g.assets         false
     end
   end
 end
